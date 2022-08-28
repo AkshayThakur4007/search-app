193701b84274a68c4208cd20574f420c0d58c330
Akshay Thakur <akshay.thakur@digimantra.com>
2022-08-28 13:27:55 +0530

    search based on multiple keys

--- components/SearchComponent/SearchComponent.tsx
+++ components/SearchComponent/SearchComponent.tsx
@@ -2,14 +2,37 @@ import react, { useState } from "react";
 
 export const SearchComponent = () => {
   const [search, setSearch] = useState("");
+  // const name = [
+  //   "Akshay",
+  //   "Ankit",
+  //   "Yash",
+  //   "Sandeep",
+  //   "Hemant",
+  //   "Vasu",
+  //   "Shubham",
+  // ];
+
   const name = [
-    "Akshay",
-    "Ankit",
-    "Yash",
-    "Sandeep",
-    "Hemant",
-    "Vasu",
-    "Shubham",
+    {
+      name: "Akshay",
+      brand: "nike",
+    },
+    {
+      name: "Ankit",
+      brand: "puma",
+    },
+    {
+      name: "Mohit",
+      brand: "Nautica",
+    },
+    {
+      name: "Yash",
+      brand: " Reebok",
+    },
+    {
+      name: "Vasu",
+      brand: "M&M",
+    },
   ];
 
   const searchHandler = (e: any) => {
@@ -26,9 +49,13 @@ export const SearchComponent = () => {
       />
       <div className="name">
         {name
-          .filter((data) => data.toLowerCase().includes(search.toLowerCase()))
+          .filter(
+            (data) =>
+              data.name.toLowerCase().includes(search.toLowerCase()) ||
+              data.brand.toLowerCase().includes(search.toLowerCase())
+          )
           .map((data, idx) => {
-            return <div key={idx}>{data}</div>;
+            return <div key={idx}>{`${data.name}- ${data.brand}`}</div>;
           })}
       </div>
     </>
